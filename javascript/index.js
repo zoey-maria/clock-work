ca113d9439385cf37d55a35bb2d7a9897bc22b4e
EvaZoey <119055944+zoey-maria@users.noreply.github.com>
2022-12-15 16:28:50 +0100

    Added a link

--- javascript/index.js
+++ javascript/index.js
@@ -22,6 +22,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -35,6 +38,7 @@ function updateCity(event) {
     "A"
   )}</small></div>
   </div>
+  <a href="/">All cities</a>
   `;
 }
 
